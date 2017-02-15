fc91cb2e16eb79f8d521d55a21921b5f066c47d1
Joery Zegers <git@jzegers.nl>
2017-02-15 13:53:21 +0100

    add logo & update favicon

--- install.sh
+++ install.sh
@@ -4,17 +4,17 @@ PS3='Heb je een .env bestand aangemaakt?'
 options=("Ja" "Nee")
 select opt in "${options[@]}"
 do
-    case $opt in
-        "Ja")
-            composer install
+	case $opt in
+		"Ja")
+			composer install
 			php artisan migrate
 			php artisan key:generate
 			read -p "De omgeving is ingesteld, druk op een toets om door te gaan"
 			[[ "$0" = "$BASH_SOURCE" ]] && exit 1 || return 1
-            ;;
-        "Nee")
+			;;
+		"Nee")
 			read -p "Maak eerst een .env bestand aan, druk op een toets om door te gaan"
 			[[ "$0" = "$BASH_SOURCE" ]] && exit 1 || return 1
-            ;;
-    esac
+			;;
+	esac
 done

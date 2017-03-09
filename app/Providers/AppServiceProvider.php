343a66f3ec81f017d002a398bd2d5f733373d4e5
Joery Zegers <git@jzegers.nl>
2017-03-09 20:28:37 +0100

    prep

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Schema;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -16,6 +17,7 @@ class AppServiceProvider extends ServiceProvider
         if (env('APP_ENV') === 'heroku') {
         $this->app['request']->server->set('HTTPS', true);
 		}
+		Schema::defaultStringLength(191);
     }
 
     /**

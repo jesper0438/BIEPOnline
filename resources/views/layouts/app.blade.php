dbf23e2b6e2780a761b23738f79db81e3625d151
Joery Zegers <git@jzegers.nl>
2017-03-10 12:10:58 +0100

    proper bootstrap 3 and remove unused css & js

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -2,15 +2,12 @@
 <html lang="en">
 <head>
 	<meta charset="utf-8">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-
+	<meta http-equiv="x-ua-compatible" content="ie=edge">
 	<title>@yield('pagetitle') | BIEPOnline</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="shortcut icon" href="{{ asset('favicon.png') }}">
-	<link href="{{ asset('css/app.css') }}" rel="stylesheet">
-	<link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
+	<link rel="stylesheet" href="{{ asset('css/app.css') }}">
 </head>
-
 <body style="padding-top: 70px;">
 <div class="container-fluid">
 	<nav class="navbar navbar-default navbar-fixed-top">

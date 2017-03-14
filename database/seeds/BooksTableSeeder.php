124a7ea4a47aced2e889fd0e6ba87ccf590ac184
Geffrey de Winter <geff@nanohost.nl>
2017-03-14 13:27:10 +0100

    composer.lock merged & seeds added

---
+++ database/seeds/BooksTableSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class BooksTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+
+        DB::table('books')->insert([
+            [
+                'isbn' => '9780545139700',
+                'title' => 'Harry Potter and the Deathly Hallows',
+                'author' => 'J.K. Rowling',
+                'created_at' => \Carbon\Carbon::now()->toDateTimeString(),
+                'updated_at' => \Carbon\Carbon::now()->toDateTimeString(),
+            ],
+
+        ]);
+    }
+}
---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert(
+            [
+                'name' => 'Administrator',
+                'email' => 'Admin@biep.dev',
+                'password' => bcrypt('Admin123!'),
+                'created_at' => \Carbon\Carbon::now()->toDateTimeString(),
+                'updated_at' => \Carbon\Carbon::now()->toDateTimeString(),
+            ]);
+    }
+}

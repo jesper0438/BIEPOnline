8a52d9411d4fce26b9f598f28575691bed428d38
Geffrey de Winter <geff@nanohost.nl>
2017-03-16 12:44:32 +0000

    Changed Categories

--- database/seeds/CategoriesTableSeeder.php
+++ database/seeds/CategoriesTableSeeder.php
@@ -11,12 +11,50 @@ class CategoriesTableSeeder extends Seeder
      */
     public function run()
     {
-        DB::table('categories')->insert(
-            [
-              'color' => 'Roze',
-              'name' => 'AVI 9',
-              'created_at' => \Carbon\Carbon::now()->toDateTimeString(),
-              'updated_at' => \Carbon\Carbon::now()->toDateTimeString(),
-            ]);
+        DB::table('categories')->insert([
+              [
+                'color' => 'Bruin',
+                'name' => 'Prentenboeken zonder tekst',
+                'created_at' => \Carbon\Carbon::now()->toDateTimeString(),
+                'updated_at' => \Carbon\Carbon::now()->toDateTimeString(),
+              ],
+              [
+                'color' => 'Rood',
+                'name' => 'Prentenboeken met tekst',
+                'created_at' => \Carbon\Carbon::now()->toDateTimeString(),
+                'updated_at' => \Carbon\Carbon::now()->toDateTimeString(),
+              ],
+              [
+                'color' => 'Blauw',
+                'name' => 'Voorleesboeken',
+                'created_at' => \Carbon\Carbon::now()->toDateTimeString(),
+                'updated_at' => \Carbon\Carbon::now()->toDateTimeString(),
+              ],
+              [
+                'color' => 'Lichtgroen',
+                'name' => 'Leesboeken voor beginners',
+                'created_at' => \Carbon\Carbon::now()->toDateTimeString(),
+                'updated_at' => \Carbon\Carbon::now()->toDateTimeString(),
+              ],
+              [
+                'color' => 'Geel',
+                'name' => 'Leesboeken onderbouw',
+                'created_at' => \Carbon\Carbon::now()->toDateTimeString(),
+                'updated_at' => \Carbon\Carbon::now()->toDateTimeString(),
+              ],
+              [
+                'color' => 'Groen',
+                'name' => 'Leesboeken middenbouw',
+                'created_at' => \Carbon\Carbon::now()->toDateTimeString(),
+                'updated_at' => \Carbon\Carbon::now()->toDateTimeString(),
+              ],
+              [
+                'color' => 'Grijs',
+                'name' => 'Leesboeken bovenbouw',
+                'created_at' => \Carbon\Carbon::now()->toDateTimeString(),
+                'updated_at' => \Carbon\Carbon::now()->toDateTimeString(),
+              ],
+
+          ]);
     }
 }

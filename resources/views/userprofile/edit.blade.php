36c20304256f83e99597450fdf52eb28597b6c87
Geffrey de Winter <geff@nanohost.nl>
2017-03-20 12:32:46 +0100

    edit.blade.php created & edited

--- resources/views/userprofile/edit.blade.php
+++ resources/views/userprofile/edit.blade.php
@@ -10,14 +10,17 @@
 <div class="form-group">
 	<div class="form-group">
 		<div class="col-sm-6">
-			{!! Form::password('old_password', ['class'=>'form-control']) !!}
-			{!! Form::password('password', ['class'=>'form-control']) !!}
-			{!! Form::password('password_confirmation', ['class'=>'form-control']) !!}
+				{!! Form::label('password', ['class' => 'control-label']) !!}
+				{!! Form::password('password', ['class' => 'form-control', 'placeholder' => 'Hier je nieuwe wachtwoord']) !!}
+		</div>
+		<div class="col-sm-6">
+			{!! Form::label('password_confirmation', ['class' => 'control-label']) !!}
+			{!! Form::password('password_confirmation', ['class'=>'form-control', 'placeholder' => 'Hier nogmaals je nieuwe wachtwoord']) !!}
 		</div>
 
 	</div>
 	<div class="form-group">
-		<div class="col-sm-12">
+		<div class="col-sm-12">name
 			<button type="submit" class="btn btn-primary">
 				Opslaan
 			</button>

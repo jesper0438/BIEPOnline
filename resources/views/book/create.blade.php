4f58a900ac0f531c45cc696eea8fa69e85178362
jesper0438 <robr0002@hz.nl>
2017-04-18 13:26:20 +0200

    Annuleer-button voor #79 Heur. 3 User contr. and freed.

--- resources/views/book/create.blade.php
+++ resources/views/book/create.blade.php
@@ -41,6 +41,7 @@
 		<button type="submit" class="btn btn-primary">
 			Opslaan
 		</button>
+		<a href="/book" class="btn btn-info" role="button">Annuleren</a>
 	</div>
 </div>
 {!! Form::close() !!}

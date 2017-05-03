d33311905656e51dab255c2dacd4483be3b59e1e
Joery Zegers <git@jzegers.nl>
2017-05-03 11:52:26 +0200

    Update DatabaseSeeder.php

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,7 +11,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $this->call(BooksTableSeeder::class);
+        //$this->call(BooksTableSeeder::class);
         $this->call(CategoriesTableSeeder::class);
         $this->call(CopiesTableSeeder::class);
         $this->call(LoansTableSeeder::class);

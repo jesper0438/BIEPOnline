0c5d646fe157ec33f5f101f0840ce7772441ad45
Joery Zegers <git@jzegers.nl>
2017-05-12 10:39:29 +0200

    remove files

--- resources/views/common/leftmenu.blade.php
+++
@@ -1,9 +0,0 @@
-<li class="nav-item">
-	<a class="nav-link" href="{!! url('book') !!}"><i class="fa fa-book fa-2x fa-fw"></i> Boeken</a>
-</li>
-<li class="nav-item">
-	<a class="nav-link" href="{!! url('loan') !!}"><i class="fa fa-arrow-circle-right fa-2x fa-fw"></i> Uitlenen</a>
-</li>
-<li class="nav-item">
-	<a class="nav-link" href="{!! url('copy') !!}"><i class="fa fa-tags fa-2x fa-fw"></i> Exemplaren</a>
-</li>
